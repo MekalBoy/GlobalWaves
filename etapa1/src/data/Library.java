cea6b619c749d69e20d07b52828d070446aca7f6
Mekal <23715925+MekalBoy@users.noreply.github.com>
2023-11-10 22:15:52 +0200

    Added Search prototype
    Search is undergoing build up at the moment, ISearchable should allow for it to easily return whatever is needed. I've still got to implement ISearchable on the actual searchable classes.

--- etapa1/src/data/Library.java
+++ etapa1/src/data/Library.java
@@ -2,21 +2,37 @@ package data;
 
 import fileio.input.LibraryInput;
 import fileio.input.SongInput;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class Library {
-    private List<AudioFile> songs;
+    private List<Song> songs;
     private List<Playlist> playlists;
     private List<Podcast> podcasts;
 
-    public Library() {}
+    public static Library instance;
+
+    private Library() {}
 
     public Library(LibraryInput input) {
-        this.songs = new ArrayList<AudioFile>();
+        this.songs = new ArrayList<Song>();
         for (SongInput songInput : input.getSongs()) {
             this.songs.add(new Song(songInput));
         }
+    }
+
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public void addPlaylist(Playlist playlist) {
+        playlists.add(playlist);
+    }
+
+    public void addPodcasts(Podcast podcast) {
+        podcasts.add(podcast);
     }
 }
--- etapa1/src/functionality/SearchBar.java
+++ etapa1/src/functionality/SearchBar.java
@@ -1,4 +1,41 @@
 package functionality;
 
+import data.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchBar {
+    public enum SearchType {
+        SONG,
+        PLAYLIST,
+        PODCAST
+    }
+
+    public class Filter {
+        public String name, album, lyrics, genre, artist, owner;
+        public int releaseYear;
+        public List<String> tags;
+    }
+
+    public List<ISearchable> Search(SearchType searchType, Filter filters) {
+        List<ISearchable> results = new ArrayList<>();
+
+        switch(searchType) {
+            case SearchType.SONG:
+                for (Song song : Library.instance.getSongs()) {
+                    
+                }
+                break;
+            case SearchType.PLAYLIST:
+                break;
+            case SearchType.PODCAST:
+                break;
+            default:
+                System.out.print("How did we get here?");
+                break;
+        }
+
+        return null;
+    }
 }
